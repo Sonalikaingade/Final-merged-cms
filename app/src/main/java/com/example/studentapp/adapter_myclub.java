e49fcb84cf5539fa9a3db980af2b0c34f2769a16
hrushikesh430 <hrishikesh430@gmail.com>
2022-06-12 18:28:44 +0530

    edited onclick

--- app/src/main/java/com/example/studentapp/adapter_myclub.java
+++ app/src/main/java/com/example/studentapp/adapter_myclub.java
@@ -3,6 +3,7 @@ package com.example.studentapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -28,6 +29,15 @@ public class adapter_myclub extends RecyclerView.Adapter<myviewholder> {
         holder.t1.setText(data.get(position).getHeader());
         holder.t2.setText(data.get(position).getDesc());
         holder.img.setImageResource(data.get(position).getImgname());
+
+
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), holder.t1.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
