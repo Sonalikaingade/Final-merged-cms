9d1a93af45df2f8d236cfd9051efdd787c6c4dbd
hrushikesh430 <hrishikesh430@gmail.com>
2022-06-12 14:41:22 +0530

    edited profile_fragment

--- app/src/main/java/com/example/studentapp/profile_fragment.java
+++ app/src/main/java/com/example/studentapp/profile_fragment.java
@@ -1,21 +1,42 @@
 package com.example.studentapp;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
+//import androidx.appcompat.widget.Toolbar;
+
+
+import com.google.android.material.navigation.NavigationView;
 
 
 public class profile_fragment extends Fragment {
 
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
+
+
+
+
         return inflater.inflate(R.layout.fragment_profile_fragment, container, false);
     }
+
 }
